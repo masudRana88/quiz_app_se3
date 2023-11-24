6a8a20b511ca44f0975cbd5cc1cbc200b31a4c68
masudRana88 <mahimuddin88@gmail.com>
2023-11-24 20:44:07 +0600

    quiz.dart and quiz_screen is create then move materialApp to quiz.dart and import quiz() into runApp()

---
+++ lib/question_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({Key? key}) : super(key: key);
+
+  @override
+  _QuestionScreenState createState() => _QuestionScreenState();
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Question screen");
+  }
+}
