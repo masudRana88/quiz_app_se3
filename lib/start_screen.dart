420bbbaa77e6ffd2a09de138e4e3d039b6629937
masudRana88 <mahimuddin88@gmail.com>
2023-11-24 14:08:30 +0600

    start_screen.dart is added

---
+++ lib/start_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Start Screen");
+  }
+}
